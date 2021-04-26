8314aa3e64ca5e17ce28f41721218335d9995120
khaybulaevan <naida-khaybulaeva@mail.ru>
2021-04-26 22:42:48 +0300

    Задание 13. Сценарий работы с корзиной

--- Task 13 - Basket.py
+++ Task 13 - Basket.py
@@ -1,10 +1,8 @@
 import pytest
 from selenium import webdriver
-import time
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from time import sleep
 
 
 options = webdriver.ChromeOptions()
@@ -19,27 +17,19 @@ def driver(request):
 
 def test_basket(driver):
     driver.get("http://localhost:8080/litecart/en/")
-    driver.implicitly_wait(10)
     for i in range(1,4):
         products = driver.find_elements_by_css_selector("div#box-most-popular ul.listing-wrapper.products li:nth-child(1)")
         for product in products:
             product.click()
-            #time.sleep(1)
             driver.find_element_by_css_selector("[name=add_cart_product]").click()
             wait = WebDriverWait(driver, 10)
             wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, "span.quantity"), str(i)))
         driver.get("http://localhost:8080/litecart/en/")
     driver.get("http://localhost:8080/litecart/en/checkout")
-    # Неявное ожидаение
-    driver.implicitly_wait(10)
-    # for i in range(1,4):
-    #     sleep(10)
+    driver.find_element_by_css_selector("ul.shortcuts li:nth-child(1)").click() # Остановить карусель
     table_of_products = driver.find_elements_by_css_selector("td.unit-cost")
     for i in range(len(table_of_products)):
-        time.sleep(2)
         driver.find_element_by_name("remove_cart_item").click()
-        # Явное ожидание
-        #time.sleep(2)
         wait = WebDriverWait(driver, 10)
         wait.until(EC.staleness_of(table_of_products[i]))
 
