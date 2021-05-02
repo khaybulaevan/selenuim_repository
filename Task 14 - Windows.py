fae4e7248a1cede88826f5b7578e112325e6a7b6
khaybulaevan <naida-khaybulaeva@mail.ru>
2021-05-02 22:22:04 +0300

    Задание 14. Проверить открытие ссылок в новом окне

--- Task 14 - Windows.py
+++ Task 14 - Windows.py
@@ -1,33 +1,34 @@
 import pytest
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
-from time import sleep
-import time
 
+options = webdriver.ChromeOptions()
+options.add_argument("start-maximized")
 
 @pytest.fixture
 def driver(request):
-    wd = webdriver.Chrome()
+    wd = webdriver.Chrome(chrome_options=options)
     request.addfinalizer(wd.quit)
     return wd
+
+
 def find_and_fill(driver, element_name, value):
     driver.find_element_by_name(element_name).click()
     driver.find_element_by_name(element_name).clear()
     driver.find_element_by_name(element_name).send_keys(value)
 
 def test_all_sections(driver):
-    driver.get("http://localhost:8080/litecart/admin/")
+    driver.get("http://localhost/litecart/admin/")
     find_and_fill(driver, element_name="username", value="admin")
     find_and_fill(driver, element_name="password", value="admin")
     driver.find_element_by_name('login').click()
     driver.implicitly_wait(10)
-    driver.get("http://localhost:8080/litecart/admin/?app=countries&doc=countries")
+    driver.get("http://localhost/litecart/admin/?app=countries&doc=countries")
     driver.find_element_by_css_selector("tr.row td:nth-child(7)").click()
-    time.sleep(1)
     all_links = driver.find_elements_by_css_selector("#content form table [target=_blank]")
     print("\nОбщее количество ссылок: " + str(len(all_links)))
-
-    #-----------------Окна------------------
-
+    # -----------------Окна------------------
     # Текущее окно
     main_window = driver.current_window_handle
     print("main_window: " + str(main_window))
@@ -37,7 +38,8 @@ def test_all_sections(driver):
     for i in range(len(all_links)):
         #print("i = " + str(i))
         all_links[i].click()
-        sleep(1)
+        wait = WebDriverWait(driver, 10)
+        wait.until(EC.new_window_is_opened(old_windows))
         # Все окна после открытия ссылки
         new_windows = driver.window_handles
         print("new_windows" + str(new_windows))
@@ -47,7 +49,7 @@ def test_all_sections(driver):
         driver.switch_to_window(new_window[0])
         driver.close()
         driver.switch_to_window(main_window)
-        sleep(1)
+
 
 
 
